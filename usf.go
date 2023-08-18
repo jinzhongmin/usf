72f118b9d21680e6ea9d0d0c11abb284976878d3
jinzhongmin <jinzm1993@163.com>
2023-08-18 19:59:09 +0800

    update

--- usf.go
+++ usf.go
@@ -10,75 +10,66 @@ import (
 	"unsafe"
 )
 
-// 得到指针存储的值
-func p2v(p unsafe.Pointer) uint64 {
-	return uint64(uintptr(p))
-}
+type (
+	_any struct {
+		size *uint64
+		addr unsafe.Pointer
+	}
+	_slice struct {
+		addr unsafe.Pointer
+		len  uint64
+		cap  uint64
+	}
+	_ptr struct{ p unsafe.Pointer }
+)
 
-// 将一个值放入指针
-func v2p(v uint64, p *unsafe.Pointer) {
-	(*(*[1]uint64)((unsafe.Pointer)(p)))[0] = v
+func AddrOf(i interface{}) unsafe.Pointer {
+	return (*_any)(unsafe.Pointer(&i)).addr
 }
-
-func anyPtr(i interface{}) (len uint64, ptr unsafe.Pointer) {
-	lp := (*(*[2]unsafe.Pointer)(unsafe.Pointer(&i)))
-	return *(*uint64)(lp[0]), lp[1]
+func SizeOf(i interface{}) uint64 {
+	return *(*_any)(unsafe.Pointer(&i)).size
 }
-
-type slice struct {
-	addr unsafe.Pointer
-	len  uint64
-	cap  uint64
+func Malloc(size uint64) unsafe.Pointer {
+	return C.malloc(C.uint64_t(size))
 }
-
-func Sizeof(i interface{}) uint64 {
-	l, _ := anyPtr(i)
-	return l
-}
-func Malloc(size uint64, typeSize uint64) unsafe.Pointer {
-	return C.malloc(C.uint64_t(size * typeSize))
+func MallocN(n uint64, typeSize uint64) unsafe.Pointer {
+	return C.malloc(C.uint64_t(n * typeSize))
 }
 func MallocOf(size uint64, zeroVal interface{}) unsafe.Pointer {
-	return C.malloc(C.uint64_t(size * Sizeof(zeroVal)))
+	return C.malloc(C.uint64_t(size * SizeOf(zeroVal)))
 }
 func Free(p unsafe.Pointer) {
+	if p == nil {
+		return
+	}
 	C.free(p)
 }
 func Memset(p unsafe.Pointer, bytev int32, bytec uint64) {
-	v := (C.int)(int32(bytev))
-	c := (C.uint64_t)(bytec)
-	C.memset(p, v, c)
+	C.memset(p, C.int(bytev), C.uint64_t(bytec))
 }
-func Memcpy(dest, src unsafe.Pointer, n uint64) { C.memcpy(dest, src, (C.uint64_t)(n)) }
+func Memcpy(dest, src unsafe.Pointer, n uint64) { C.memcpy(dest, src, C.uint64_t(n)) }
 func Slice(p unsafe.Pointer, n uint64) unsafe.Pointer {
-	s := &slice{addr: p, len: n, cap: n}
-	return unsafe.Pointer(s)
+	return unsafe.Pointer(&_slice{addr: p, len: n, cap: n})
 }
 
-func Push(dst unsafe.Pointer, src unsafe.Pointer) {
-	(*(*[1]uint64)(dst))[0] = p2v(src)
-}
+func Push(dst unsafe.Pointer, src unsafe.Pointer)   { (*_ptr)(dst).p = src }
+func Pop(ptr unsafe.Pointer) (inPtr unsafe.Pointer) { return (*_ptr)(ptr).p }
 func PushAt(list unsafe.Pointer, idx uint64, ptr unsafe.Pointer) {
-	p := unsafe.Add(list, idx*8)
-	(*(*[1]uint64)(p))[0] = p2v(ptr)
-}
-func Pop(ptr unsafe.Pointer) (inPtr unsafe.Pointer) {
-	return (*(*[1]unsafe.Pointer)(ptr))[0]
+	(*_ptr)(unsafe.Add(list, idx*8)).p = ptr
 }
 func PopAt(list unsafe.Pointer, idx uint64) (ptrAddr unsafe.Pointer) {
-	p := unsafe.Add(list, idx*8)
-	return (*(*[1]unsafe.Pointer)(p))[0]
+	return (*_ptr)(unsafe.Add(list, idx*8)).p
 }
 
-func Ptrs(clist unsafe.Pointer, num uint64) []unsafe.Pointer {
-	return *(*[]unsafe.Pointer)(Slice(clist, num))
-}
-func Clist(src []unsafe.Pointer) unsafe.Pointer {
-	if src == nil {
-		panic("src could not be nil")
-	}
-	dst := Malloc(uint64(len(src)), 8)
-	p := *(*[]unsafe.Pointer)(Slice(dst, uint64(len(src))))
-	copy(p, src)
-	return dst
-}
+// func Ptrs(clist unsafe.Pointer, num uint64) []unsafe.Pointer {
+// 	return *(*[]unsafe.Pointer)(Slice(clist, num))
+// }
+// func Clist(src []unsafe.Pointer) unsafe.Pointer {
+// 	if src == nil {
+// 		return nil
+// 	}
+// 	dst := Malloc(uint64(len(src)), 8)
+// 	p := *(*[]unsafe.Pointer)(Slice(dst, uint64(len(src))))
+// 	copy(p, src)
+// 	return dst
+// }
